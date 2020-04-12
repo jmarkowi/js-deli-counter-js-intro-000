b6fdc606d8ade6ee0eae3773a414e7a0a6600a8c
Jesse Markowitz <jess.markowitz@gmail.com>
2020-04-12 02:30:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(katzDeliLine, newPerson) {
+function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(`${newperson}`);
   return `Welcome, ${newperson}. You are number ${katzDeliLine.length + 1} in line.`
 }
