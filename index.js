6908184daa156dadf0b23f2ef545fda7d52f61ff
Jesse Markowitz <jess.markowitz@gmail.com>
2020-04-12 02:41:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length = 0) {
     return "There is nobody waiting to be served!";
   }
-  let first = katzDeliLine[0]
+  var first = katzDeliLine[0]
   katzDeliLine.shift()
   return `Currently serving ${first}.`
 }
